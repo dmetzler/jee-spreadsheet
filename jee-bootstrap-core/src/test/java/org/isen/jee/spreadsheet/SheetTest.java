e963dc044adfda146910943fc5efdc2bd2b742cc
Damien Metzler <dmetzler@nuxeo.com>
2017-01-09 13:53:11 +0100

    First dummy test

---
+++ jee-bootstrap-core/src/test/java/org/isen/jee/spreadsheet/SheetTest.java
@@ -0,0 +1,15 @@
+package org.isen.jee.spreadsheet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class SheetTest {
+
+
+
+	@Test
+	public void can_instanciate_a_spreadSheet() throws Exception {
+		assertThat(true).isTrue();
+	}
+}
